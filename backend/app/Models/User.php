2ff2179058ebcdd5c7c4068aae2c3d23469d926e
Laith Al Enooz <laithalenooz@gmail.com>
2021-09-30 17:03:28 +0300

    added user phase, adding image upload then go for the admin phase

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Wallet;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends = ['balance', 'walletId', 'expenses'];
+
+    public function getBalanceAttribute()
+    {
+        $total = auth()->user()->wallet->balances->sum('amount');
+        $expenses = auth()->user()->wallet->expenses->sum('amount');
+        return $total - $expenses;
+    }
+
+    public function getExpensesAttribute()
+    {
+        return auth()->user()->wallet->expenses->sum('amount');
+    }
+
+    public function getWalletIdAttribute()
+    {
+        return auth()->user()->wallet->id;
+    }
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class, 'user_id');
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::resources([
+    'wallet'   => App\Http\Controllers\BalancesController::class,
+    'expenses' => App\Http\Controllers\ExpensesController::class
+]);
+
+require __DIR__.'/auth.php';
