cd579a2ee73c9b1d18219d4548e4b719db643631
Laith Al Enooz <laithalenooz@gmail.com>
2021-10-01 12:09:44 +0300

    added users seeder

---
+++ backend/database/seeders/UsersSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class UsersSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+                'name'     => 'Admin',
+                'email'    => 'admin@ingot.com',
+                'image'    => 'user.png',
+                'password' => bcrypt('password'),
+                'phone'    => '00962777777777',
+                'birthdate'=> '1999-11-11'
+        ]);
+
+        DB::table('users')->insert([
+            'name'     => 'User',
+            'email'    => 'user@ingot.com',
+            'image'    => 'user.png',
+            'password' => bcrypt('password'),
+            'phone'    => '00962777777777',
+            'birthdate'=> '1999-11-11'
+        ]);
+    }
+}
