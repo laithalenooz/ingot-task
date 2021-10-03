a9643a27e43b5d24bdb8d8078ed2e7e0e8e8dec8
Laith Al Enooz <laithalenooz@gmail.com>
2021-10-03 11:30:07 +0300

    missing commit

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
             'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'image'    => ['required', 'image', 'max:5120', 'mimes:jpeg,png,jpg,gif,svg'],
-            'phone'    => ['required']
+            'phone'    => ['required', 'unique:users']
         ]);
 
         $imageName = time().'.'.$request->image->extension();
